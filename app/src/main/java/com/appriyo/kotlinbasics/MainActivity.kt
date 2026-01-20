03a11a5312be3e86147b4831f8a27b3b38989008
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-01-20 22:05:23 +0600

    Initial commit

---
+++ app/src/main/java/com/appriyo/kotlinbasics/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.appriyo.kotlinbasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloWorld()
+        }
+    }
+}
+
+val cat = 5
+val dog = 10
+var men = 11
+@Composable
+fun HelloWorld(){
+    Text("Hello World.")
+    Text("Cat $cat Dog $dog Men $men")
+}
